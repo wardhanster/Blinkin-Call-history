4b2fff31c5ed5e058aff71072dc02693d1767dee
ranjith <ranjith.v@blinkin.io>
2020-04-17 17:00:33 +0530

    updated loading component with spinner

--- src/components/CallLogs/CallLogs.js
+++ src/components/CallLogs/CallLogs.js
@@ -1,39 +1,47 @@
 import React from "react";
 import CallLog from "./CallLog/CallLog";
 import "./CallLogs.css";
-import { Table } from "reactstrap";
+
+import { Table, Spinner } from "reactstrap";
 
 const callLogs = (props) => {
-  console.log(props.content);
   const tableRows = props.content.data.map((cont) => {
-    return <CallLog {...cont} showImage={props.showImage} />;
+    return <CallLog key={cont.room_id} {...cont} showImage={props.showImage} />;
   });
 
   return (
-    <Table style={{ width: 1000, margin: "auto" }}>
-      <thead className="calllogs__thead">
-        <tr>
-          <th>
-            <i class="fa fa-id-card-o" aria-hidden="true"></i> Room ID
-          </th>
-          <th>
-            <i class="fa fa-calendar" aria-hidden="true"></i> Start Date
-          </th>
-          <th>
-            <i class="fa fa-hourglass-end" aria-hidden="true"></i> Start Time
-          </th>
-          <th>
-            <i class="fa fa-clock-o" aria-hidden="true"></i> Duration
-          </th>
-          <th>
-            <i class="fa fa-phone" aria-hidden="true"></i> To
-          </th>
+    <>
+      {props.loading && (
+        <div className="text-center m-4">
+          <Spinner style={{ width: "3rem", height: "3rem" }} />
+        </div>
+      )}
+      <Table style={{ width: 1000, margin: "auto" }}>
+        <thead className="calllogs__thead">
+          <tr>
+            <th>
+              <i className="fa fa-id-card-o" aria-hidden="true"></i> Room ID
+            </th>
+            <th>
+              <i className="fa fa-calendar" aria-hidden="true"></i> Start Date
+            </th>
+            <th>
+              <i className="fa fa-hourglass-end" aria-hidden="true"></i> Start
+              Time
+            </th>
+            <th>
+              <i className="fa fa-clock-o" aria-hidden="true"></i> Duration
+            </th>
+            <th>
+              <i className="fa fa-phone" aria-hidden="true"></i> To
+            </th>
 
-          <th>Files</th>
-        </tr>
-      </thead>
-      <tbody>{tableRows}</tbody>
-    </Table>
+            <th>Files</th>
+          </tr>
+        </thead>
+        <tbody>{tableRows}</tbody>
+      </Table>
+    </>
   );
 };
 
