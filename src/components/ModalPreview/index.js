fca93ee511fe216c062dc68ac073852850f6b602
ranjith <ranjith.v@blinkin.io>
2020-04-21 23:55:22 +0530

    updated return for all and converted all file format to lower case

--- src/components/ModalPreview/index.js
+++ src/components/ModalPreview/index.js
@@ -10,6 +10,7 @@ import {
   Card,
   CardTitle,
   CardText,
+  Spinner,
 } from "reactstrap";
 import classnames from "classnames";
 
@@ -40,70 +41,26 @@ let video = [
   "video",
 ];
 
+function Loading() {
+  return (
+    <div className="text-center m-4">
+      <Spinner style={{ width: "3rem", height: "3rem" }} />
+    </div>
+  );
+}
+
 export default function ModalPreview(props) {
   const [activeTab, setActiveTab] = useState("1");
+  const [loading, setLoading] = useState(true);
 
   const toggle = (tab) => {
     if (activeTab !== tab) setActiveTab(tab);
   };
 
-  let returnFiles = (fileType) => {
-    let files = props.previewData.map((item, index) => {
-      if (fileType === "image") {
-        if (image.indexOf(item.file_extension) > -1) {
-          let presentUrl = `${props.fileBasePath}${item.file_name}.${item.file_extension}`;
-          return (
-            <div key={index}>
-              <img
-                className="img-fluid"
-                alt={`${index}_image`}
-                src={presentUrl}
-              />
-            </div>
-          );
-        } else {
-          return <p key={index}>No Images Avaliable</p>;
-        }
-      }
-
-      if (fileType === "video") {
-        if (video.indexOf(item.file_extension) > -1) {
-          let presentUrl = `${props.fileBasePath}${item.file_name}.${item.file_extension}`;
-          return (
-            <video key={index} width="320" height="240" controls>
-              <source src={presentUrl} type="video/mp4" />
-              <source src={presentUrl} type="video/ogg" />
-              Your browser does not support the video tag.
-            </video>
-          );
-        } else {
-          return <p key={index}>No Videos Avaliable</p>;
-        }
-      }
-
-      if (fileType === "others") {
-        if (
-          video.indexOf(item.file_extension) <= -1 &&
-          image.indexOf(item.file_extension) <= -1
-        ) {
-          let presentUrl = `${props.fileBasePath}${item.file_name}.${item.file_extension}`;
-          return (
-            <Col key={index} sm="6">
-              <Card body>
-                <CardTitle>{item.orginal_name}</CardTitle>
-                <CardText>File - {item.file_extension}</CardText>
-                <a rel="noopener noreferrer" href={presentUrl} target="_blank">
-                  Goto File location
-                </a>
-              </Card>
-            </Col>
-          );
-        } else {
-          return <p key={index}>No Files Avaliable</p>;
-        }
-      }
-    });
-    return files;
+  let stopLoading = () => {
+    if (loading) {
+      setLoading(false);
+    }
   };
 
   return (
@@ -141,20 +98,90 @@ export default function ModalPreview(props) {
         </NavItem>
       </Nav>
       <TabContent activeTab={activeTab}>
+        {loading && <Loading />}
         <TabPane tabId="1">
           <Row>
             <Col sm="12">
               <div className="tab-all preview_image">
-                {returnFiles("image")}
+                {props.previewData
+                  .filter(
+                    (e) =>
+                      image.indexOf(e.file_extension.toLocaleLowerCase()) !== -1
+                  )
+                  .map((imageItem, index) => {
+                    let presentUrl = `${props.fileBasePath}${imageItem.file_name}.${imageItem.file_extension}`;
+                    return (
+                      <img
+                        key={`${index}_image`}
+                        className="img-fluid"
+                        alt={`${index}_image`}
+                        onLoad={stopLoading}
+                        src={presentUrl}
+                      />
+                    );
+                  })}
+                {props.previewData.filter(
+                  (e) =>
+                    image.indexOf(e.file_extension.toLocaleLowerCase()) !== -1
+                ) && "No Images Exist"}
               </div>
             </Col>
           </Row>
         </TabPane>
         <TabPane tabId="2">
-          <div className="tab-all">{returnFiles("video")}</div>
+          <div className="tab-all">
+            {props.previewData
+              .filter(
+                (e) =>
+                  video.indexOf(e.file_extension.toLocaleLowerCase()) !== -1
+              )
+              .map((item, index) => {
+                let presentUrl = `${props.fileBasePath}${item.file_name}.${item.file_extension}`;
+                return (
+                  <video key={index} width="320" height="240" controls>
+                    <source src={presentUrl} type="video/mp4" />
+                    <source src={presentUrl} type="video/ogg" />
+                    Your browser does not support the video tag.
+                  </video>
+                );
+              })}
+            {props.previewData.filter(
+              (e) => video.indexOf(e.file_extension.toLocaleLowerCase()) !== -1
+            ) && "No Video Files Exist"}
+          </div>
         </TabPane>
         <TabPane tabId="3">
-          <div className="tab-all">{returnFiles("others")}</div>
+          <div className="tab-all">
+            {props.previewData
+              .filter(
+                (e) =>
+                  video.indexOf(e.file_extension.toLocaleLowerCase()) !== -1 &&
+                  image.indexOf(e.file_extension.toLocaleLowerCase()) !== -1
+              )
+              .map((item, index) => {
+                let presentUrl = `${props.fileBasePath}${item.file_name}.${item.file_extension}`;
+                return (
+                  <Col key={index} sm="6">
+                    <Card body>
+                      <CardTitle>{item.orginal_name}</CardTitle>
+                      <CardText>File - {item.file_extension}</CardText>
+                      <a
+                        rel="noopener noreferrer"
+                        href={presentUrl}
+                        target="_blank"
+                      >
+                        Goto File location
+                      </a>
+                    </Card>
+                  </Col>
+                );
+              })}
+            {props.previewData.filter(
+              (e) =>
+                video.indexOf(e.file_extension.toLocaleLowerCase()) !== -1 &&
+                image.indexOf(e.file_extension.toLocaleLowerCase()) !== -1
+            ) && "No Files Exist"}
+          </div>
         </TabPane>
       </TabContent>
     </div>
