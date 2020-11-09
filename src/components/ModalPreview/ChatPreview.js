e1694d7ea9f43fb77ea49652bc9f00c4afc398a0
ranjith <ranjith.v@blinkin.io>
2020-11-09 11:55:16 +0530

    updated design

--- src/components/ModalPreview/ChatPreview.js
+++ src/components/ModalPreview/ChatPreview.js
@@ -7,7 +7,7 @@ export default function ChatPreview({ msg }) {
     <div className="jumbotron m-0 p-0 bg-transparent">
       <div className="row m-0 p-0 ">
         <div className="col-7 p-0 m-0 right-0 mx-auto">
-          <div className="card border-0 rounded rounded-item">
+          <div className="border-0 rounded rounded-item">
             <div className="card bg-sohbet border-0 m-0 p-0 h-100-val">
               <div
                 id="sohbet"
