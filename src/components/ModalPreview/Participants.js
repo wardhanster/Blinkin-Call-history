598b052cd7b8c7e5d07339fb6d399bd35963ec19
ranjith <ranjith.v@blinkin.io>
2020-11-12 16:08:01 +0530

    updated design

--- src/components/ModalPreview/Participants.js
+++ src/components/ModalPreview/Participants.js
@@ -9,9 +9,15 @@ export default function Participants({ participants }) {
             <div key={`par_${key}`} className="col">
               <div className="card">
                 <div className="card-body">
-                  <p className="card-text">{item.name}</p>
-                  <p className="card-text">{item.uid}</p>
-                  <p className="card-text">{item.ua}</p>
+                  <p className="card-text">
+                    <b>Name</b> - {item.name}
+                  </p>
+                  <p className="card-text">
+                    <b>UserId</b> - {item.uid}
+                  </p>
+                  <p className="card-text">
+                    <b>User Agent</b> - {item.ua}
+                  </p>
                 </div>
               </div>
             </div>
