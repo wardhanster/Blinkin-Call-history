23fe77f741bd690643be535b95fe218ee2c9c359
ranjith <ranjith.v@blinkin.io>
2020-11-12 16:02:02 +0530

    updated design

--- src/components/ModalPreview/Participants.js
+++ src/components/ModalPreview/Participants.js
@@ -2,10 +2,21 @@ import React from "react";
 
 export default function Participants({ participants }) {
   return (
-    <div>
-      {JSON.parse(participants.participants).map((item, key) => {
-        return <p key={`par_${key}`}>{item.name}</p>;
-      })}
+    <div className="row">
+      {participants &&
+        JSON.parse(participants.participants).map((item, key) => {
+          return (
+            <div key={`par_${key}`} className="col">
+              <div className="card">
+                <div className="card-body">
+                  <p className="card-text">{item.name}</p>
+                  <p className="card-text">{item.uid}</p>
+                  <p className="card-text">{item.ua}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 }
